ddaff4aa50506fc1fe8d5d4bec2a959bda6257f1
Everton Lima <everton.lima@witec.com.br>
2020-10-13 14:14:38 -0300

    Projeto completo

---
+++ models/Player.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+
+const Player = new mongoose.Schema({
+	firstName: {type: String, default:''},
+	lastName: {type: String, default:''},
+	position: {type: String, default:''},
+	age: {type: Number, default:0},
+	team: {type: String, default:''},
+	password: {type:String}
+})
+
+module.exports = mongoose.model('Player', Player)
